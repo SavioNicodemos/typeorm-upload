f17b4309c6c4fc67340d9111f36f5c675cfc22d0
Savio Nicodemos <saviogomes_182@hotmail.com>
2021-01-13 07:44:21 -0300

    Desafio Finalizado

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,57 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import Category from '../models/Category';
+
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
+
+    if (type === 'outcome') {
+      const { total } = await transactionsRepository.getBalance();
+
+      if (total < value) {
+        throw new AppError('Invalid balance for this operation');
+      }
+    }
+
+    const hasCategory = await categoriesRepository.findOne({ title: category });
+
+    if (!hasCategory) {
+      const newCategory = categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(newCategory);
+    }
+
+    const getCategory = await categoriesRepository.findOne({ title: category });
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: getCategory,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
